376a65296702d6f833a8f5fc080abb6d16e7f747
swalla50 <stevenwallacejr@aol.com>
2022-01-19 20:39:11 -0500

    make it better

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import userRoutes from './routes/users.js';
 
 const app = express();
+
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
@@ -14,11 +17,15 @@ app.use(cors());
 
 app.use('/users', userRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://admin:Miami2002@altbookcluster.icqbb.mongodb.net/altbooksDB?retryWrites=true&w=majority';
+app.get('/', (req, res) =>{
+    res.send('Hello to Users API');
+});
+
+// const CONNECTION_URL = 'mongodb+srv://admin:Miami2002@altbookcluster.icqbb.mongodb.net/altbooksDB?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+    .catch((error) => console.log(`${error} did not connect`));
 
 //https://www.mongodb.com/cloud/atlas
